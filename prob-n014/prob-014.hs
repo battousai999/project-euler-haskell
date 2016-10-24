7355e6e9513db406766cc8f6776d00fa00f7be1c
Kurtis Jones <battousai999@gmail.com>
2016-10-24 08:15:38 -0600

    Implemented problem #14.

---
+++ prob-n014/prob-014.hs
@@ -0,0 +1,35 @@
+-- The following iterative sequence is defined for the set of positive integers:
+
+-- n → n/2 (n is even)
+-- n → 3n + 1 (n is odd)
+
+-- Using the rule above and starting with 13, we generate the following sequence:
+
+-- 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+-- It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+-- Which starting number, under one million, produces the longest chain?
+
+-- NOTE: Once the chain starts the terms are allowed to go above one million.
+
+import Data.List (maximumBy)
+import Data.Function.Memoize (memoFix)
+
+collatzHelper :: (Integer -> [Integer]) -> Integer -> [Integer]
+collatzHelper f x
+    | x == 1    = [1]
+    | even x    = x : f (x `div` 2)
+    | otherwise = x : f (3 * x + 1)
+
+collatzSeq :: Integer -> [Integer]
+collatzSeq num =
+    let helper = memoFix collatzHelper
+    in  helper num
+
+calcValue :: Integer
+calcValue =
+    fst $ maximumBy (\x y -> (snd x) `compare` (snd y)) $ map (\x -> (x, (length . collatzSeq) x)) $ takeWhile (< 1000000) [1..]
+
+main = do
+    putStrLn $ show $ calcValue 
+
