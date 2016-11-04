0fe6708fb68d30760bc4e6e3996a59cb9e56ce2b
Kurtis Jones <battousai999@gmail.com>
2016-11-04 17:41:10 -0600

    Implemented problem #24.

---
+++ prob-n024/prob-024.hs
@@ -0,0 +1,20 @@
+-- A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+-- 012   021   102   120   201   210
+
+-- What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+import Data.List (sort, foldl')
+
+permutate :: Ord a => [a] -> [[a]]
+permutate [] = [[]]
+permutate xs =
+    let sortedXs = sort xs
+        subPerms n = 
+            let subXs = filter (\x -> x /= n) xs
+            in  map (n:) $ permutate subXs 
+    in  concat $ map subPerms sortedXs
+
+main = do
+    let join xs = foldl' (++) "" $ map show xs
+    putStrLn $ join $ permutate [0,1,2,3,4,5,6,7,8,9] !! 999999
